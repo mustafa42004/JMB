01f44de322a8b578f086fb3d41040567e2857aa9
Sadiq53 <jafarussadiq098@gmail.com>
2025-03-06 15:50:58 +0530

    change the url

--- JBM Enterprises/src/util/API_URL.js
+++ JBM Enterprises/src/util/API_URL.js
@@ -1,6 +1,6 @@
 // const API_URL = "https://jmb-enterprise.onrender.com/admin" //render
-const API_URL = "http://localhost:8080/admin" //local
+// const API_URL = "http://localhost:8080/admin" //local
 // const API_URL = "https://dolphin-app-i8qlu.ondigitalocean.app/admin" digiocean
-// const API_URL = "https://api.jmb-enterprises.in/admin"
+const API_URL = "https://api.jmb-enterprises.in/admin"
 
 export {API_URL};
